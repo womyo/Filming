8427aaa648b77e1c1624d6ac08a93b41d81f2b0c
Lee Inho <inhoking01@gmail.com>
2024-05-20 10:42:11 +0900

    Initial Commit

---
+++ Filming/FilmingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmingApp.swift
+//  Filming
+//
+//  Created by 이인호 on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct FilmingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
