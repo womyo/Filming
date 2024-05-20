e458bf3fedbceb01301ee446cd3def7923994855
Lee Inho <inhoking01@gmail.com>
2024-05-21 00:48:52 +0900

    feat. add SceneKit View

---
+++ Filming/SceneKitView.swift
@@ -0,0 +1,38 @@
+//
+//  SceneKitView.swift
+//  Filming
+//
+//  Created by 이인호 on 5/20/24.
+//
+
+import SwiftUI
+import SceneKit
+
+struct SceneKitView: UIViewRepresentable {
+    var modelName: String
+    
+    func makeUIView(context: Context) -> SCNView {
+        let scnView = SCNView()
+        scnView.allowsCameraControl = true
+        scnView.autoenablesDefaultLighting = true
+        
+        if let scene = SCNScene(named: modelName) {
+            if let node = scene.rootNode.childNodes.first {
+                node.eulerAngles = SCNVector3(0, 0, 0)
+            }
+            scnView.scene = scene
+        } else {
+            print("Error: Could not load the 3D model named \(modelName)")
+        }
+        
+        return scnView
+    }
+    
+    func updateUIView(_ uiView: SCNView, context: Context) {
+        
+    }
+}
+
+#Preview {
+    SceneKitView(modelName: "")
+}
---
+++ Filming/SplashScreenView.swift
@@ -0,0 +1,44 @@
+//
+//  SplashScreenView.swift
+//  Filming
+//
+//  Created by 이인호 on 5/20/24.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isActive = false
+    
+    var body: some View {
+        if isActive {
+            ContentView()
+        } else {
+            ZStack {
+                Color(UIColor(red: 0.95, green: 0.92, blue: 0.85, alpha: 1.0)).ignoresSafeArea()
+                VStack {
+                    Image(systemName: "camera.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                    Text("Filming")
+                        .font(.title)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundStyle(.gray)
+                        .padding()
+                }
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                        withAnimation {
+                            self.isActive = true
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
